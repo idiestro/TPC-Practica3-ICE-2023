2061e51f5be34c322e151fdadbf403e70ea04e88
idiestro <nachodiestrogil@gmail.com>
2023-11-26 13:06:33 +0100

    FunctionsI fix search method

--- src/server1/FunctionsI.java
+++ src/server1/FunctionsI.java
@@ -6,6 +6,9 @@ import Ice.Current;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,7 +16,7 @@ public class FunctionsI extends ServerFunctions._Server1Disp{
 	
 	private static final long serialVersionUID = 1L;
 	private HashMap<String, String> clientData;
-	private String [] txtFileData;
+	private String[] txtFileData;
 	private String txtFilePath = (System.getProperty("user.dir") + "/Resources/DB/PretendingBeDB.txt");
 	
 			
@@ -41,22 +44,23 @@ public class FunctionsI extends ServerFunctions._Server1Disp{
 	@Override
 	public String txtReader(Current current) {
 		// TODO Auto-generated method stub
-
-        try {
-        	//Create reader for file
-        	BufferedReader reader = new BufferedReader(new FileReader(txtFilePath));
-        	//Create variables to save data
-        	String line;
-        	//While data (file) exist read info
-        	while((line = reader.readLine()) != null) {
-    			//Save txt info into txtFileData
-    			txtFileData = line.split(" ");
-        	}
-        	reader.close();
-        //Input/Output exception control
-        } catch (IOException e) {
-        		System.err.println("Error al trabajar con archivo .txt: " + e.getMessage());
-        }
+		try {
+			//Init arraylist
+			txtFileData = new ArrayList<>();
+			//Create reader for file
+			BufferedReader reader = new BufferedReader(new FileReader(txtFilePath));
+			//Create variables to save data
+			String line;
+			//While data (file) exist read info
+			while((line = reader.readLine()) != null) {
+				//Save txt info into txtFileData
+				txtFileData.add(line);
+			}
+			reader.close();
+			//Input/Output exception control
+		} catch (IOException e) {
+			System.err.println("Error al trabajar con archivo .txt: " + e.getMessage());
+		}
 		return null;
 	}
 	
@@ -67,19 +71,19 @@ public class FunctionsI extends ServerFunctions._Server1Disp{
 	public void compareInputWithSaveData(BooleanHolder result, Current current) {
 		// TODO Auto-generated method stub
 		try {
-			//Declare boolean for search result
-			result.value =
-				//Search name, firstSurname and secondSurname coincidence
-				(txtFileData[0].toLowerCase().contains(clientData.get("name").toLowerCase())) && 
-				(txtFileData[1].toLowerCase().contains(clientData.get("firstSurname").toLowerCase())) &&
-				(txtFileData[2].toLowerCase().contains(clientData.get("secondSurname").toLowerCase()));
+			for (String txtFileDatum : txtFileData) {
+				//Declare boolean for search result
+				if (txtFileDatum.toLowerCase().contains(clientData.get("name").toLowerCase()) || txtFileDatum.toLowerCase().contains(clientData.get("firstSurname").toLowerCase())) {
+					result = true;
+					break;
+				}
+			}
 
-			
 		}catch(Exception e) {
-    		System.err.println("Error al comparar datos de cliente y PretendingBeDB.txt:" + e.getMessage());
-    		result.value = false;
+			System.err.println("Error al comparar datos de cliente y PretendingBeDB.txt:" + e.getMessage());
+			result = false;
 		}
-		
+
 	}
 
 
